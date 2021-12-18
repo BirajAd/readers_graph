921714ff19a3164fd5102a81bc9a2f1fec919e0f
sandescrst <sandescrst@gmail.com>
2021-12-17 19:05:16 -0600

    merge fixed

--- follow/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.follow, name= "follow"),
-    path("follower", views.followers, name="follower"),
-    path("followee", views.followees, name="followee")
-]
--- follow/views.py
+++ follow/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.db import models
 
 # Create your views here.
-class Follow(models.Model):
-    follower_id = models.CharField()
+
