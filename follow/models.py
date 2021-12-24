1b0c8f42ab548803a69c5bdf9ad75eba620626e5
Biraj Adhikari <adhikari.biraj1@gmail.com>
2021-12-23 23:27:02 -0600

    migrations related fixing

--- follow/models.py
+++ follow/models.py
@@ -6,6 +6,6 @@ from users.models import User
 from django.utils import timezone
 
 class Follow(models.Model):
-    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followerId')
-    followee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followeeId')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='list_followers')
+    followee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='list_followees')
     timestamp = models.DateTimeField(default=timezone.now())
