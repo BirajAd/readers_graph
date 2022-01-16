cab38106f486dcaffb5e39d386f76f33b186f80b
BirajAd <adhikari.biraj1@gmail.com>
2022-01-16 17:06:47 -0600

    unnecessary commented code removed

--- users/views.py
+++ users/views.py
@@ -9,20 +9,6 @@ from posts.models import Post
 from rest_framework.response import Response
 from datetime import datetime
 from django.db.models import F
-# from users.serialization import UserSerializer 
-
-# Create your views here.
-# class Login(APIView):
-#     def post(self, request, *args, **kwargs):
-#         is_authenticated = authenticate(self.request.POST.get("username"), self.request.POST.get("password"))
-#         resp = {}
-#         if(is_authenticated):
-#             resp["status"] = True
-#             resp["details"]["key"] = Token.objects.get(user=request.POST.user).key
-#             resp["user"] = User.objects.get(user=request.POST.user)
-#             return Response(resp)
-#         else:
-#             return Response({"status": False, "details": "either email or password is wrong."})
 
 
 class CustomAuthToken(ObtainAuthToken):
@@ -62,5 +48,3 @@ class CreateUser(APIView):
             "token": token.key,
             "user": list(result_user)
         })
-
-
