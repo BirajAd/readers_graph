a06977751e5796ec4daa50c3640fc796c3eda467
BirajAd <adhikari.biraj1@gmail.com>
2022-03-13 21:10:17 -0500

    register route fixed to return jwt token

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from users.models import User
 from rest_framework.response import Response
-from datetime import datetime
+from datetime import date, datetime
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -70,12 +70,13 @@ class UserInfo(APIView):
             "user": user
         })
         
-class CreateUser(APIView):
+class CreateUser(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         email = self.request.data.get('email')
         username = self.request.data.get('username')
         password = self.request.data.get('password')
         date_of_birth = self.request.data.get('date_of_birth')
+        print(date_of_birth)
         if not password:
             return Response({
                 "status": False,
@@ -83,8 +84,9 @@ class CreateUser(APIView):
             })
         anUser = User.objects.create_user(email, password, date_of_birth, username)
         result_user = User.objects.filter(pk=anUser.id).values('id', 'username', 'last_active', 'first_name', 'last_name', 'bio', 'profile_picture', 'email', 'gender', 'organization', 'city', 'country', 'is_superuser')
-        token, created = Token.objects.get_or_create(user=anUser)
+        token = MyTokenObtainPairSerializer.get_token(anUser)
         return Response({
-            "token": token.key,
+            "access": str(token.access_token),
+            "refresh": str(token),
             "user": list(result_user)
         })
