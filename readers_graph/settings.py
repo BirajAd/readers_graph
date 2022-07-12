6e8d55aeaea10f68c55eab1eede82e7e16f1e944
Sandesh Shrestha <43733567+sandescrst@users.noreply.github.com>
2022-07-12 11:47:48 -0500

    Update settings.py

--- readers_graph/settings.py
+++ readers_graph/settings.py
@@ -18,8 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y6q0gfh0kj+ac8n1j&ubiq*02+9=uc$i5)l17dreielz+v$cd_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
